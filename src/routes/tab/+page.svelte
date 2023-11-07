a02cde912fd2b9631cf434a253f362f8f197470e
Carlos Lopez <cloaction@gmail.com>
2023-11-07 20:49:31 +0100

    clear and page demo

---
+++ src/routes/tab/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Vex } from "https://cdn.skypack.dev/vexflow@beta/build/esm/entry/vexflow.js";
+	import { onMount } from "svelte";
+
+  // Vex.Flow.setMusicFont("Bravura");
+
+  onMount(() => {
+    const factory = new Vex.Flow.Factory({
+      renderer: { elementId: "vextab", width: 500, height: 130 },
+    });
+    const score = factory.EasyScore();
+    factory
+        .System()
+        .addStave({
+            voices: [score.voice(score.notes("C#5/q, B4, A4, G#4", { stem: "up" })), score.voice(score.notes("C#4/h, C#4", { stem: "down" }))],
+        })
+        .addClef("treble")
+        .addTimeSignature("4/4");
+    factory.draw();
+  })
+
+</script>
+
+<div id="vextab"></div>
